dfc12f80a5823407fe08fa9ac07418203e1131b2
AnatoliMitkov <sprgtxnx@gmail.com>
2026-04-22 16:58:46 +0300

    up

--- app/layout.jsx
+++ app/layout.jsx
@@ -128,6 +128,16 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en" data-page-motion="on" suppressHydrationWarning>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HS1V41ZYQ4" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-HS1V41ZYQ4');`,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,400&family=Inter:wght@300;400;500&display=swap" rel="stylesheet" />
